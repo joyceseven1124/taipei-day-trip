a839409bb123e0555d355a724948c04c07547644
MSI\劉佳怡 <sevenjoyce1124@gmail.com>
2022-11-29 15:52:02 +0800

    feat: every attraction introduces

---
+++ taipei-day-trip/static/attraction.js
@@ -0,0 +1,123 @@
+let dataNet = window.location.href
+let dataNetArray = dataNet.split("/")
+let idNumber = parseInt(dataNetArray[4])
+let img_number = 0 
+
+getAttraction(idNumber)
+
+function getAttraction(id){
+        url="/api/attraction/"+id
+        fetch(url)
+        .then(function(response){
+            return response.json();})
+        .then(function(data){
+            let result = data["data"][0]
+            img_number = result["images"].length
+            let imgPlace = document.querySelector(".attraction_information_images_main")
+            let imgCircle = document.querySelector(".attraction_information_images_circle")
+            for(let i=0; i < result["images"].length; i++){
+                let img = document.createElement("img")
+                img.setAttribute("class","image")
+                img.src = result["images"][i]
+                imgPlace.appendChild(img)
+
+                let circle = document.createElement("span")
+                circle.setAttribute("class","circle")
+                circle.setAttribute("num",i)
+                if(i===0){
+                    circle.classList.add("checked")
+                }
+                imgCircle .appendChild(circle)
+            }
+
+            let attractionName = document.querySelector(".attraction_information_booking_name_title")
+            attractionName.textContent = result["name"]
+
+            let attractionCategory = document.querySelector(".attraction_information_booking_name_category")
+            attractionCategory.textContent = result["category"]
+            
+            let attractionDescription = document.querySelector("#description")
+            attractionDescription.textContent = result["description"]
+
+            let attractionAddress = document.querySelector("#address")
+            attractionAddress.textContent = result["address"]
+
+            let attractionTransport = document.querySelector("#transport")
+            attractionTransport.textContent = result["transport"]
+
+        })
+}
+
+
+
+
+let circleIndex=0;
+let previous = document.getElementsByClassName("previous")
+previous[0].onclick = function () {
+    circleIndex--;
+    if(circleIndex == -1){
+        circleIndex = img_number-1
+    }
+    slideTo(circleIndex)
+}
+
+let next = document.getElementsByClassName("next");
+next[0].onclick = function () {
+    circleIndex++;
+    if(circleIndex  < img_number){
+        slideTo(circleIndex)
+    }else if(circleIndex == img_number){
+        circleIndex = 0
+        slideTo(circleIndex)
+    }
+}
+
+function slideTo(index){
+    let images = document.getElementsByClassName('image');
+    for(let i=0;i<images.length;i++){
+        if( i == index ){
+            images[i].style.display = 'inline'
+            images[i].style.animation = "fadein 1s ease"
+        }else{
+            images[i].style.display = 'none'
+        }
+    }
+
+    let circle = document.getElementsByClassName("circle");
+    for(let j=0;j<circle.length;j++){
+        if( j == index ){
+            circle[j].classList.add("checked")
+            circleIndex=j
+        }else{
+            circle[j].classList.remove("checked")
+        }
+    }
+}
+
+
+
+let price = document.querySelector("#price")
+let morningTime = document.querySelector("#morning_time")
+let afternoonTime = document.querySelector("#afternoon_time")
+
+morningTime.addEventListener('change',(e)=>{
+    if(e.target.checked){
+        price.textContent="2000"
+    }else{
+        price.textContent="2500"
+    }
+})
+
+afternoonTime.addEventListener("change",(e)=>{
+    if(e.target.checked){
+        price.textContent="2500"
+    }
+})
+
+
+function goHome(){
+    window.location.href = "/"
+}
+
+let home =  document.querySelector(".navigation_name")
+home.addEventListener("click",goHome)
--- taipei-day-trip/static/index.js
+++ taipei-day-trip/static/index.js
@@ -21,73 +21,72 @@ const callback = (entries,observer) => {
 const observer = new IntersectionObserver(callback, options)
 
 function getData(page,string){
-        if(state){
-            state = false
-            url="/api/attractions?page="+page+"&keyword="+string
+    if(state){
+        state = false
+        url="/api/attractions?page="+page+"&keyword="+string
         fetch(url)
-    .then(function(response){
-        return response.json();})
-    .then(function(jsonData){
-        let result =jsonData
-        if(result["data"].length !== 0){
-            let attractions = document.querySelector('.attractions');
-            for(let x = 0; x < result["data"].length; x++){
-                let attractionCard =document.createElement("div")
-                attractionCard.setAttribute("class","attractions_container")
-                attractions.appendChild(attractionCard)
-            }
+        .then(function(response){
+            return response.json();})
+        .then(function(jsonData){
+            let result =jsonData
+            if(result["data"].length !== 0){
+                let attractions = document.querySelector('.attractions');
+                for(let x = 0; x < result["data"].length; x++){
+                    let attractionCard =document.createElement("div")
+                    attractionCard.setAttribute("class","attractions_container")
+                    attractionCard.setAttribute("id",result["data"][x]["id"])
+                    attractions.appendChild(attractionCard)
+                }
 
-            let attractionCardPlace = document.querySelectorAll('.attractions_container')
-            for(let i = 0; i <result["data"].length ; i++){
-                let picture = result["data"][i]["images"][0]
-                let attractionName = result["data"][i]["name"]
+                let attractionCardPlace = document.querySelectorAll('.attractions_container')
+                for(let i = 0; i <result["data"].length ; i++){
+                    let picture = result["data"][i]["images"][0]
+                    let attractionName = result["data"][i]["name"]
 
-                let attractionImg =document.createElement("img")
-                attractionImg.setAttribute("class","attractions_container_img")
-                attractionImg.src = picture
-                attractionCardPlace[(page*12)+i].appendChild(attractionImg)
+                    let attractionImg =document.createElement("img")
+                    attractionImg.setAttribute("class","attractions_container_img")
+                    attractionImg.src = picture
+                    attractionCardPlace[(page*12)+i].appendChild(attractionImg)
 
-                let attractionWord =document.createElement("div")
-                attractionWord.setAttribute("class","attractions_container_word")
-                attractionWord.setAttribute("index",attractionName)
-                attractionCardPlace[(page*12)+i].appendChild(attractionWord)
-            }
+                    let attractionWord =document.createElement("div")
+                    attractionWord.setAttribute("class","attractions_container_word")
+                    attractionWord.setAttribute("index",attractionName)
+                    attractionCardPlace[(page*12)+i].appendChild(attractionWord)
+                }
 
-            let attractionWordPlace = document.querySelectorAll('.attractions_container_word');
-            for(let y = 0; y < result["data"].length; y++){
-                let category = result["data"][y]["category"]
-                let mrt = result["data"][y]["mrt"]
-                let attractionMRT = document.createElement("div")
-                let attractionMRTName = document.createTextNode(mrt)
-                attractionMRT.appendChild(attractionMRTName)
+                let attractionWordPlace = document.querySelectorAll('.attractions_container_word');
+                for(let y = 0; y < result["data"].length; y++){
+                    let category = result["data"][y]["category"]
+                    let mrt = result["data"][y]["mrt"]
+                    let attractionMRT = document.createElement("div")
+                    let attractionMRTName = document.createTextNode(mrt)
+                    attractionMRT.appendChild(attractionMRTName)
 
-                let attractionCategory = document.createElement("div")
-                let attractionCategoryName = document.createTextNode(category)
-                attractionCategory.appendChild(attractionCategoryName)
+                    let attractionCategory = document.createElement("div")
+                    let attractionCategoryName = document.createTextNode(category)
+                    attractionCategory.appendChild(attractionCategoryName)
 
-                attractionWordPlace[(page*12)+y].appendChild(attractionMRT)
-                attractionWordPlace[(page*12)+y].appendChild(attractionCategory)
-            }
-            state = true
-        }else{
-            let noResult = document.querySelector('.attractions_no_result')
-            noResult.textContent = "查無此資料"
+                    attractionWordPlace[(page*12)+y].appendChild(attractionMRT)
+                    attractionWordPlace[(page*12)+y].appendChild(attractionCategory)
+                }
+                state = true
+            }else{
+                let noResult = document.querySelector('.attractions_no_result')
+                noResult.textContent = "查無此資料"}
+            nextPage = result["nextPage"]
+            return nextPage
+        })
+        .then(function(nextPage){
+            if(nextPage !== null && state){
+                state = true
+                observer.observe(target)
+            }else{
+                observer.disconnect()
+                nextPage = null}
+        })
+        .catch(function(err){console.log(err)})
         }
-        return result["nextPage"]
-    })
-    .then(function(Page){
-        if(Page !== null && state){
-            state = true
-            nextPage = nextPage+1
-            observer.observe(target)
-        }else{
-            observer.disconnect()
-            nextPage = 0
-        }
-    })
-    .catch(function(err){console.log(err)})
     }
-}
 
 function categorySelect(){
     fetch("/api/categories")
@@ -109,7 +108,6 @@ getData(0,"")
 categorySelect()
 
 
-
 function keywordLoad(page,string){
         let hint = document.querySelector(".home_word_search_hint")
         hint.textContent = ""
@@ -125,7 +123,6 @@ function keywordLoad(page,string){
 
 function clickEffect(e){
     clickWhere = e.target.nodeName
-    console.log(clickWhere)
     if(clickWhere === "INPUT"){
         let menu = document.querySelector("#menu").classList
         if(menu.contains("hide")){
@@ -137,7 +134,6 @@ function clickEffect(e){
         menu.classList.add("hide")
     }else if(clickWhere === "IMG"){
         menu.classList.add("hide")
-        console.log(e.target.parentNode.nodeName)
         if(e.target.parentNode.nodeName === "BUTTON"){
             let menu = document.querySelector("#menu").classList.add("hide")
             let inputPlace = document.querySelector(".home_word_search_input")
@@ -158,9 +154,18 @@ function clickEffect(e){
             inputPlace.value =""
             keyword = ""
             nextPage = 0
-            keywordLoad(nextPage,keyword)}
+            keywordLoad(0,keyword)}
     }else{
         menu.classList.add("hide")}
     }
+
+function directNewPlace(e){
+    let attractionCardNumber = e.target.parentNode.attributes["id"].value
+    window.location.href = "/attraction/"+attractionCardNumber
+}
+
 const all = document.querySelector("body")
 all.addEventListener("click",clickEffect)
+
+const attractionNet = document.querySelector(".attractions")
+attractionNet.addEventListener("click", directNewPlace)
